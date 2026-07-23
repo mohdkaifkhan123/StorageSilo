47af60ef7f76de07d5371dd5b7540e69f357ade0
mohdkaifkhan123 <khank11111999@gmail.com>
2026-07-23 21:33:53 +0530

    Changing the delete flow

--- server/routes/folderRoutes.js
+++ server/routes/folderRoutes.js
@@ -1,4 +1,4 @@
-import { createFolder, shareFolder,getFolderContent,deleteFolder } from "../controller/folderController.js";
+import { createFolder, shareFolder, getFolderContent, deleteFolder, restoreFolder } from "../controller/folderController.js";
 import { Router } from "express";
 import { protect } from "../middleware/protect.js";
 import { findFoldersFiles } from "../controller/folderController.js";
@@ -6,8 +6,9 @@ const router = Router();
 
 router.post("/create", protect, createFolder);
 router.get("/", protect, findFoldersFiles);
-router.post("/share",protect,shareFolder);
-router.get("/allcontent/:id",protect,getFolderContent);
-router.post("/delete/:id",protect,deleteFolder);
+router.post("/share", protect, shareFolder);
+router.get("/allcontent/:id", protect, getFolderContent);
+router.post("/delete/:id", protect, deleteFolder);
+router.post("/restore/:id", protect, restoreFolder);
 
 export default router;
